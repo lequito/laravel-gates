1370b269a7da1629e7a5b6a5a8026cb88651023f
Lekito <alexdossantosdias1212@gmail.com>
2025-03-04 02:38:35 -0300

    creation of home and main view

---
+++ resources/views/components/layouts/main-layout.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('assets/bootstrap/bootstrap.min.css') }}">
+    <title>Laravel Gates</title>
+</head>
+<body>
+    
+    <div class="container mt-5">
+        <p class="display-6">Laravel Gates</p>
+
+        @auth
+            <p><span class="text-info me-3">{{ Auth::user()->username }}</span><a href="#"></a></p>
+        @else    
+            <p class="opacity-75">Nenhum usuário logado</p>
+        @endauth
+    </div>
+
+    {{ $slot }}
+    <script src="asset('assets/bootstrap/bootstrap.bundle.min.js'"></script>
+</body>
+</html>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+<x-layouts.main-layout>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <li>Item 1</li>
+                <li>Item 2</li>
+                <li>Item 3</li>
+            </div>
+        </div>
+    </div>
+</x-layouts.main-layout>
