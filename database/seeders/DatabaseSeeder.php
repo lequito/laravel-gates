b5221adb6870a3bdc649a0f65991f561b34cb0a0
Lekito <alexdossantosdias1212@gmail.com>
2025-03-04 07:54:18 -0300

    preparing database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,11 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name'     => 'User',
+            'email'    => 'test@mail.com',
+            'password' => bcrypt('Aa123456'),
+            'role'     => 'admin',
+            'permissions' => 'admin'
         ]);
     }
 }
