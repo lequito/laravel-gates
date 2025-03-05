1d7e5ff3dcd5150d07c5af219ab8b7af754c69db
Lekito <alexdossantosdias1212@gmail.com>
2025-03-05 01:43:47 -0300

    using blade directives for gates

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,11 +7,43 @@
                     @guest
                         <li><a href="{{ route('login') }}">Login</a></li>
                     @else
-                        <li><a href="{{ route('only_admin') }}">Somente admins</a></li>    
-                        <li><a href="{{ route('only_users') }}">Somente usuários</a></li>    
+                        @can('userIsAdmin')
+                            <li><a href="{{route('only_admin') }}">Somente admins</a></li>    
+                        @endcan
+
+                        @can('userIsUser')
+                            <li><a href="{{ route('only_users') }}">Somente usuários</a></li>    
+                        @endcan
                     @endguest
                 </ul>
             </div>
         </div>
+   
+        <div class="row">
+            <div class="col">
+                @can('userIsAdmin')
+                    <p>Admin</p>
+                @endcan
+    
+                @can('userIsAdmin')
+                    <p>Admin</p>
+                @else
+                    <p>User sem privilégio</p>
+                @endcan
+    
+                @cannot('userIsAdmin')
+                    <p>OK</p>
+                @endcannot
+            </div>
+        </div>
+
+        @canany(['userIsAdmin', 'userIsUser'])
+            <p>Olá!</p>
+        @endcanany
     </div>
+
+
+    
+
+    
 </x-layouts.main-layout>
